84f2b8ebec83820a33d00e20f182bd50b96f3fd0
Abdullah Zidan <abdullahzidan62@gmail.com>
2026-02-06 18:26:57 -0500

    feat: normalize urls

--- normalize_url.go
+++ normalize_url.go
@@ -1,3 +1,20 @@
-func normalizeURL() string {
-	return ""
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func normalizeURL(inputURL string) (string, error) {
+	urlStruct, err := url.Parse(inputURL)
+	if err != nil {
+		return "", err
+	}
+	s := (fmt.Sprintf("%s%s", urlStruct.Host, urlStruct.Path))
+	length := len(s)
+	if s[length-1] == '/' {
+		s = s[:length-1]
+	}
+
+	return s, nil
 }
